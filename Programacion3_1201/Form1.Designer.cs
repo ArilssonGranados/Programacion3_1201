74e7d59aa2e9579b7f0e1397125b9793cd994d18
ArilssonGranados <86398351+ArilssonGranados@users.noreply.github.com>
2021-07-02 11:05:21 -0600

    Primer comit
    Todo el ejercicio

---
+++ Programacion3_1201/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace Programacion3_1201
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFila = new System.Windows.Forms.TextBox();
+            this.txtColumna = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.dgvMatriz = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese el tamaño de la matriz:";
+            // 
+            // txtFila
+            // 
+            this.txtFila.Location = new System.Drawing.Point(169, 51);
+            this.txtFila.Name = "txtFila";
+            this.txtFila.Size = new System.Drawing.Size(53, 20);
+            this.txtFila.TabIndex = 1;
+            // 
+            // txtColumna
+            // 
+            this.txtColumna.Location = new System.Drawing.Point(238, 51);
+            this.txtColumna.Name = "txtColumna";
+            this.txtColumna.Size = new System.Drawing.Size(53, 20);
+            this.txtColumna.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fila:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Columna:";
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(447, 133);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(114, 58);
+            this.btnEjecutar.TabIndex = 5;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // dgvMatriz
+            // 
+            this.dgvMatriz.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatriz.Location = new System.Drawing.Point(16, 133);
+            this.dgvMatriz.Name = "dgvMatriz";
+            this.dgvMatriz.Size = new System.Drawing.Size(425, 272);
+            this.dgvMatriz.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(447, 242);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 58);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Limpiar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(447, 347);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 58);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 417);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvMatriz);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtColumna);
+            this.Controls.Add(this.txtFila);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFila;
+        private System.Windows.Forms.TextBox txtColumna;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.DataGridView dgvMatriz;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Programacion3_1201/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programacion3_1201
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Declaro la matriz
+        int[,] matriz;
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            //Declaro dos variables para las filas y las columnas y las inicializo con los textbox correspondiente
+            int fila = int.Parse(txtFila.Text);
+            int columna = int.Parse(txtColumna.Text);
+
+            //Declaro una variable de tipo random
+            Random aleatorio = new Random();
+
+            //Le doy el tamaño a la matriz con las dimensiones ingresadas por el usuario
+            matriz = new int[fila, columna];
+
+            //Le asigno las dimensiones que tendra el datagridview con los datos ingresados por el usuario
+            dgvMatriz.ColumnCount = columna;
+            dgvMatriz.RowCount = fila;
+
+            //Aquí guardo los números random dentro de la matriz en dos for anidados
+            for (int i = 0; i < fila; i++) //Este for será para el conteo de filas 
+            {
+                for (int j = 0; j < columna; j++) //Este será para el conteo de columnas
+                {
+                    matriz[i, j] = aleatorio.Next(100); //Aquí almaceno los números aleatorios en el DataGridView
+                }
+
+            }
+
+            //Estos for anidados serán para mostrar los números almacenados en la matriz
+            for (int i = 0; i < fila; i++) //For para conteo de filas
+            {
+                for (int j = 0; j < columna; j++)//For para conteo de columnas
+                {
+                    dgvMatriz.Rows[i].Cells[j].Value = matriz[i, j]; //Muestro los datos de la matriz al DataGridView
+                }
+            }
+
+        }
+
+        //Botón para limpiar los textbox y el DataGridView
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtFila.Clear();
+            txtColumna.Clear();
+            dgvMatriz.Rows.Clear();
+            dgvMatriz.Columns.Clear();
+            txtFila.Focus();
+        }
+
+        //Botón para Cerrar el Form
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
